734c773dc45f8e29b556e2a6128f699443cae9b2
Syamjith Sasidharan <syamjithas@gmail.com>
2021-11-12 08:23:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UseStateExample from "./Components/UseStateExample";
+
+function App() {
+  return (
+    <div>
+      <UseStateExample initVal={0} />
+    </div>
+  );
+}
+
+export default App;
